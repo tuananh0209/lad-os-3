23e09d305495a9d516fbf792af47e6b55c0214dd
tuananh0209 <huynhtuananh0209@gmail.com>
2020-04-29 00:04:32 +0700

    complete

--- cau 2/ex2.c
+++ cau 2/ex2.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
+#include <sys/wait.h>
 
 int main()
 {
-    fork();
-    fork();
-    fork();
+   
+    pid_t pr1 = fork();
+    wait(NULL);
+    pid_t pr2 = fork();
+    wait(NULL);
+    pid_t pr3 = fork();
+    wait(NULL);
+    printf("[son] pid %d from [parent] pid %d\n", getpid(), getppid());
+    return 1;
 }
